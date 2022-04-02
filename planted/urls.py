984108f4df1c075c60e1f5e2cfa76aa48e5aae91
kfusilier <kristinfusilier@gmail.com>
2022-04-02 17:44:56 -0600

    server running, home page view works, database connected

--- planted/urls.py
+++ planted/urls.py
@@ -1,3 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include # <- you must add include to the imports
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main_app.urls')) # <- here is the new line to include the urls of our app
+]
+
 """planted URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +21,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
