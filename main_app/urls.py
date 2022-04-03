864bae3dc05d1832dbf62ab3661a66841cf6a467
kfusilier <kristinfusilier@gmail.com>
2022-04-02 18:43:36 -0600

    templates, urls & views set for: about, base, home, note-detail, plant-detail, plant-list & profile pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,14 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
 	path('', views.Home.as_view(), name="home"),
-	path('about/', views.About.as_view(), name='about'),
+	path('about/', views.About.as_view(), name='about'), 
+	path('plants/', views.Plant_List.as_view(), name='plant_list'), 
+	path('profile/', views.Profile.as_view(), name='profile'), 
+	path('plant_detail/', views.Plant_Detail.as_view(), name='plant_detail'), 
+	path('note_detail/', views.Note_Detail.as_view(), name='note_detail'), 
+	
 ]
+
+	# path('signup/', views.About.as_view(), name='signup'), 
+	# path('login/', views.About.as_view(), name='login'), 
+	# path('', views.About.as_view(), name='logout'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
-from django.views import View # <- View class to handle requests
-from django.http import HttpResponse # <- a class to handle sending a type of response
-
+from django.views import View # class to handle requests
+from django.http import HttpResponse # class to handle sending a type of response
+#...
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
 # Here we will be creating a class called Home and extending it from the View class
-class Home(View):
+class Home(TemplateView):
+    template_name = "home.html"
 
-    def get(self, request):
-        # Here we are returning a generic response
-        # This is similar to response.send() in express
-        return HttpResponse("Planted Home")
+class About(TemplateView):
+    template_name = "about.html"
 
-class About(View):
-    def get(self, request):
-        return HttpResponse('About Page')
+class Plant_List(TemplateView):
+    template_name = "plant_list.html"
+
+class Profile(TemplateView):
+    template_name = "profile.html"
+
+class Plant_Detail(TemplateView):
+    template_name = "plant_detail.html"
+
+class Note_Detail(TemplateView):
+    template_name = "note_detail.html"
