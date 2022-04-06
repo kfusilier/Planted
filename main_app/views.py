ab9e44288d08de0d97b67b76d4e49557ada393e2
kfusilier <kristinfusilier@gmail.com>
2022-04-05 21:56:42 -0600

    search function works on plant list

--- main_app/views.py
+++ main_app/views.py
@@ -8,27 +8,34 @@ from .models import Plant
 
 # Here we will be creating a class called Home and extending it from the View class
 class Home(TemplateView):
-    template_name = "home.html"
+	template_name = "home.html"
 
 class About(TemplateView):
-    template_name = "about.html"
+	template_name = "about.html"
 
 class Profile(TemplateView):
-    template_name = "profile.html"
+	template_name = "profile.html"
 
 class Plant_Detail(TemplateView):
-    template_name = "plant_detail.html"
+	template_name = "plant_detail.html"
 
 class Note_Detail(TemplateView):
-    template_name = "note_detail.html"
+	template_name = "note_detail.html"
 
 class Calendar(TemplateView): 
-    template_name = "calendar.html"
+	template_name = "calendar.html"
 
 class Plant_List(TemplateView):
-    template_name = "plant_list.html"
+	template_name = "plant_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["plants"] = Plant.objects.all() # Here we are using the model to query the database for us.
-        return context
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		# to get the query parameter we have to acccess it in the request.GET dictionary object        
+		kind = self.request.GET.get("kind")
+		# If a query exists we will filter by kind
+		if kind != None:
+		# .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
+			context["plants"] = Plant.objects.filter(kind__icontains=kind)
+		else:
+			context["plants"] = Plant.objects.all()
+		return context
