f8a527b33e1b2a8996447d79c9bf0ba38fff5d32
kfusilier <kristinfusilier@gmail.com>
2022-04-11 16:37:03 -0600

    deploy: configure deployment

--- planted/settings.py
+++ planted/settings.py
@@ -38,7 +38,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = [planted-app.herokuapp.com]
+ALLOWED_HOSTS = [
+    'planted-app.herokuapp.com'
+    ]
 
 # Application definition
 
